3afd96774e9133aabedb2fa3a1d81b9d66f44db5
Anton Kulyk <kuliks.anton@gmail.com>
2021-07-16 18:02:33 +0300

    Test official badge is shown throughout the app

--- frontend/test/metabase/scenarios/collections/collection-types.cy.spec.js
+++ frontend/test/metabase/scenarios/collections/collection-types.cy.spec.js
@@ -5,6 +5,17 @@ import {
   describeWithToken,
   describeWithoutToken,
 } from "__support__/e2e/cypress";
+import { SAMPLE_DATASET } from "__support__/e2e/cypress_sample_dataset";
+
+const { ORDERS, ORDERS_ID } = SAMPLE_DATASET;
+
+const COLLECTION_NAME = "Official Collection Test";
+
+const TEST_QUESTION_QUERY = {
+  "source-table": ORDERS_ID,
+  aggregation: [["count"]],
+  breakout: [["field", ORDERS.CREATED_AT, { "temporal-unit": "hour-of-day" }]],
+};
 
 describeWithToken("collections types", () => {
   beforeEach(() => {
@@ -18,13 +29,13 @@ describeWithToken("collections types", () => {
     // Test can create official collection
     cy.icon("new_folder").click();
     modal().within(() => {
-      cy.findByLabelText("Name").type("Official Collection Test");
+      cy.findByLabelText("Name").type(COLLECTION_NAME);
       setOfficial();
       cy.button("Create").click();
     });
-    cy.findByText("Official Collection Test").click();
+    cy.findByText(COLLECTION_NAME).click();
     cy.findByTestId("official-collection-marker");
-    assertSidebarIcon("Official Collection Test", "badge");
+    assertSidebarIcon(COLLECTION_NAME, "badge");
 
     // Test can change regular collection to official
     cy.icon("pencil").click();
@@ -34,7 +45,7 @@ describeWithToken("collections types", () => {
       cy.button("Update").click();
     });
     cy.findByTestId("official-collection-marker").should("not.exist");
-    assertSidebarIcon("Official Collection Test", "folder");
+    assertSidebarIcon(COLLECTION_NAME, "folder");
 
     // Test can change regular collection to official
     cy.icon("pencil").click();
@@ -44,7 +55,11 @@ describeWithToken("collections types", () => {
       cy.button("Update").click();
     });
     cy.findByTestId("official-collection-marker");
-    assertSidebarIcon("Official Collection Test", "badge");
+    assertSidebarIcon(COLLECTION_NAME, "badge");
+  });
+
+  it("displays official badge throughout the application", () => {
+    testOfficialBadgePresence();
   });
 });
 
@@ -72,16 +87,55 @@ describeWithoutToken("collection types", () => {
   });
 
   it("should not display official collection icon", () => {
-    cy.createCollection({
-      name: "Official Collection Test",
-      authority_level: "official",
-    });
-    cy.visit("/collection/root");
-    cy.findByText("Official Collection Test").click();
-    cy.findByTestId("official-collection-marker").should("not.exist");
-    assertSidebarIcon("Official Collection Test", "folder");
+    testOfficialBadgePresence(false);
   });
 });
+
+function testOfficialBadgePresence(expectBadge = true) {
+  cy.createCollection({
+    name: COLLECTION_NAME,
+    authority_level: "official",
+  }).then(response => {
+    const { id: collectionId } = response.body;
+    cy.createQuestion({
+      name: "Official Question",
+      collection_id: collectionId,
+      query: TEST_QUESTION_QUERY,
+    });
+    cy.createDashboard("Official Dashboard", { collection_id: collectionId });
+    cy.visit(`/collection/${collectionId}`);
+  });
+
+  // Collections page
+  cy.findByTestId("official-collection-marker").should(
+    expectBadge ? "exist" : "not.exist",
+  );
+  assertSidebarIcon(COLLECTION_NAME, expectBadge ? "badge" : "folder");
+
+  // Dashboard Page
+  cy.findByText("Official Dashboard").click();
+  assertHasCollectionBadge(expectBadge);
+
+  // Question Page
+  cy.findByText(COLLECTION_NAME).click();
+  cy.findByText("Official Question").click();
+  assertHasCollectionBadge(expectBadge);
+
+  // Search
+  cy.get(".Nav")
+    .findByPlaceholderText("Search…")
+    .as("searchBar")
+    .type("Official");
+
+  cy.findByTestId("search-results-list").within(() => {
+    assertSearchResultBadge(COLLECTION_NAME, {
+      expectBadge,
+      selector: "h3",
+    });
+    assertSearchResultBadge("Official Question", { expectBadge });
+    assertSearchResultBadge("Official Dashboard", { expectBadge });
+  });
+}
 
 function setOfficial(official = true) {
   const isOfficialNow = !official;
@@ -106,5 +160,22 @@ function assertSidebarIcon(collectionName, expectedIcon) {
     .parent()
     .within(() => {
       cy.icon(expectedIcon);
+    });
+}
+
+function assertSearchResultBadge(itemName, opts) {
+  const { expectBadge } = opts;
+  cy.findByText(itemName, opts)
+    .parentsUntil("[data-testid=search-result-item]")
+    .within(() => {
+      cy.icon("badge").should(expectBadge ? "exist" : "not.exist");
+    });
+}
+
+function assertHasCollectionBadge(expectBadge = true) {
+  cy.findByText(COLLECTION_NAME)
+    .parent()
+    .within(() => {
+      cy.icon("badge").should(expectBadge ? "exist" : "not.exist");
     });
 }
